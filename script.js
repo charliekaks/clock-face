04a0425d7dd728879a3da98cb079877f9cba5e15
charles Kakai <kaksunchained@gmail.com>
2023-05-01 21:53:52 +0300

    Implemented a script that simulates an analog watch on the browser.

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+//Get the elements from the html svg with query selector
 const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
+
+function runClock(){
+    //Get the current date in hours, minutes and seconds
+ let date = new Date();
+ let hrs = date.getHours();
+ let min = date.getMinutes();
+ let sec = date.getSeconds();
+
+//Get the relative position of the clock hands
+let hrPosition = (hrs *(360/12) )+ (min*(360/60)/12);
+let minPosition = (min*(360/60)) + (sec*(360/60)/60);
+let secPosition =( sec*(360/60));
+
+//move the clock hands
+HOURHAND.style.transform = "rotate("+ hrPosition +"deg)";
+MINUTEHAND.style.transform = "rotate("+ minPosition +"deg)";
+SECONDHAND.style.transform = "rotate("+ secPosition +"deg)";
+
+}
+//set interval to call the function every second
+let interval = setInterval(runClock, 1000);
